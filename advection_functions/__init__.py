68f5a4adef28917669951d4885d1a038ac7c1130
“geoffreypugsley” <gjpugsley@gmail.com>
2024-04-22 19:59:32 +0100

    modify init

--- advection_functions/__init__.py
+++ advection_functions/__init__.py
@@ -1,8 +1,1 @@
-import os
-import logging
-
-#logging.getLogger("csat2").addHandler(logging.NullHandler())
-
-# user_location - the default location for the user override file
-user_location = os.path.expanduser("~/.advection_functions/config.cfg")
 
